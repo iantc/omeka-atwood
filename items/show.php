84ec52296000038c1269e5457e4b39e9d13f07ef
Ian Chan <ichan@csusm.edu>
2013-01-07 00:30:32 -0800

    Layout and added functionality for item level display pages

--- items/show.php
+++ items/show.php
@@ -3,32 +3,39 @@ echo head(array('title' => $title, 'bodyid' => 'exhibit', 'bodyclass' => 'exhibi
 ?>
 <div class="row">
   <div class="twelve columns">
-    <div id="primary">  
-      <h1 class="item-title"><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
-        <div id="itemfiles">
-		  <?php echo files_for_item($options = array('imageSize' => 'fullsize'));?>
-        </div>
-		<?php if (metadata('item', 'Collection Name')): ?>
-          <div id="collection" class="field">
-          <h2><?php echo __('Collection'); ?></h2>
-            <div class="field-value"><p><?php echo link_to_collection_for_item(); ?></p></div>
-          </div>
-		<?php endif; ?>    
-		<?php if (metadata('item', 'has tags')): ?>
-          <div class="tags">
-          <h2><?php echo __('Tags'); ?></h2>
-		  <?php echo tag_string('item'); ?>
-          </div>
-		<?php endif;?>    
-        <div id="citation" class="field">
-          <h4><?php echo __('Citation'); ?></h4>
-          <p id="citation-value" class="field-value"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></p>
-        </div>
-		<?php $description = metadata($item, array('Dublin Core', 'Description'), array('snippet' => 150));?>
-        <?php if ($description): ?>
-          <p class="item-description"><?php echo $description; ?></p>
-        <?php endif; ?>
+    <h1 class="item-title"><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
+  </div>
+</div>
+<div class="row">
+  <div class="six columns">
+    <div id="primary">
+      <div id="itemfiles">
+  		  <?php echo files_for_item($options = array('imageSize' => 'fullsize'));?>
+      </div>
     </div>
+  </div>
+  <div class="six columns">
+    <?php echo link_to_related_exhibits($item);?>
+		<?php if (metadata('item', 'Collection Name')): ?>
+      <div id="collection" class="field">
+        <h4><?php echo __('Collection'); ?></h4>
+          <div class="field-value"><p><?php echo link_to_collection_for_item(); ?></p></div>
+      </div>
+		<?php endif; ?>
+		<?php if (metadata('item', 'has tags')): ?>
+      <div class="tags">
+        <h2><?php echo __('Tags'); ?></h2>
+  		  <?php echo tag_string('item'); ?>
+      </div>
+		<?php endif;?>
+    <div id="citation" class="field">
+      <h4><?php echo __('Citation'); ?></h4>
+      <p id="citation-value" class="field-value"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></p>
+    </div>
+		<?php $description = metadata($item, array('Dublin Core', 'Description'), array('snippet' => 150));?>
+    <?php if ($description): ?>
+      <p class="item-description"><?php echo $description; ?></p>
+    <?php endif; ?>
   </div>
 </div>
 <?php echo foot(); ?>
