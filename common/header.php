a93c31147a1d4127a8a7b0a3993539b0cf47f01a
Ian Chan <ichan@csusm.edu>
2013-01-09 22:45:24 -0800

    Tweak slideshow settings

--- common/header.php
+++ common/header.php
@@ -11,7 +11,7 @@
 <!-- Stylesheets -->
 <?php
 queue_css_file(array('app', 'foundation', 'local'));
-queue_css_url('http://fonts.googleapis.com/css?family=PT+Serif:400,700,400italic,700italic');
+queue_css_url('//fonts.googleapis.com/css?family=PT+Serif:400,700,400italic,700italic|Trykker|Montserrat');
 echo head_css();
 ?>
 <!-- JavaScripts -->
@@ -20,23 +20,23 @@ echo head_css();
 <script type="text/javascript">
   jQuery(window).load(function(){
     jQuery('#featured').orbit({
-      animation: 'horizontal-slide',                  // fade, horizontal-slide, vertical-slide, horizontal-push
-      animationSpeed: 800,                // how fast animtions are
+      animation: 'fade',                  // fade, horizontal-slide, vertical-slide, horizontal-push
+      animationSpeed: 1000,                // how fast animtions are
       timer: true,                        // true or false to have the timer
-      resetTimerOnClick: false,           // true resets the timer instead of pausing slideshow progress
-      advanceSpeed: 4000,                 // if timer is enabled, time between transitions
-      pauseOnHover: false,                // if you hover pauses the slider
+      resetTimerOnClick: true,           // true resets the timer instead of pausing slideshow progress
+      advanceSpeed: 6000,                 // if timer is enabled, time between transitions
+      pauseOnHover: true,                // if you hover pauses the slider
       startClockOnMouseOut: false,        // if clock should start on MouseOut
       startClockOnMouseOutAfter: 1000,    // how long after MouseOut should the timer start again
       directionalNav: true,               // manual advancing directional navs
       captions: true,                     // do you want captions?
       captionAnimation: 'fade',           // fade, slideOpen, none
       captionAnimationSpeed: 800,         // if so how quickly should they animate in
-      bullets: true,                     // true or false to activate the bullet navigation
+      bullets: false,                     // true or false to activate the bullet navigation
       bulletThumbs: false,                // thumbnails for the bullets
       bulletThumbLocation: '',            // location from this file where thumbs will be
       afterSlideChange: function(){},     // empty function
-      fluid: false                         // or set a aspect ratio for content slides (ex: '4x3')
+      fluid: true                         // or set a aspect ratio for content slides (ex: '4x3')
     });
   })
   jQuery(document).ready(function(){
