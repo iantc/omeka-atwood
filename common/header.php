c5cd116007439d37f8a4e4d2527c4deef1e39404
Ian C <ian@ik2.us>
2014-10-04 19:12:15 -0700

    Update for compatibility with Foundation 5.4.0

--- common/header.php
+++ common/header.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
+<html class="no-js" lang="en" >
 <head>
   <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo option('site_title'); echo isset($title) ? ' | ' . $title : ''; ?></title>
   <!-- Meta -->
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -13,40 +13,15 @@
   <?php fire_plugin_hook('public_head', array('view'=>$this)); ?>
   <!-- Stylesheets -->
 
-  <link href="//cdnjs.cloudflare.com/ajax/libs/foundation/4.1.6/css/normalize.min.css" media="all" rel="stylesheet" type="text/css" >
-  <link href="//cdnjs.cloudflare.com/ajax/libs/foundation/4.1.6/css/foundation.min.css" media="all" rel="stylesheet" type="text/css" >
+  <link href="//cdnjs.cloudflare.com/ajax/libs/foundation/5.4.0/css/normalize.min.css" media="all" rel="stylesheet" type="text/css" >
+  <link href="//cdnjs.cloudflare.com/ajax/libs/foundation/5.4.0/css/foundation.min.css" media="all" rel="stylesheet" type="text/css" >
   <?php
   queue_css_file(array('local'));
   queue_css_url('//fonts.googleapis.com/css?family=PT+Serif:400,700,400italic,700italic|Trykker|Montserrat');
   echo head_css();
   ?>
   <!-- JavaScripts -->
-  <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/foundation/4.1.6/js/vendor/custom.modernizr.min.js"></script>
-  <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/foundation/4.1.6/js/vendor/jquery.min.js"></script>
-  <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/foundation/4.1.6/js/foundation.min.js"></script>
-  <script type="text/javascript">
-    jQuery(document).ready(function() {
-      jQuery('#site-header .navigation').addClass('right');
-      jQuery('#secondary-nav .navigation').addClass('side-nav')
-      jQuery('#search-form input[type=submit]').addClass('button expand postfix');
-      jQuery('#atwood-layout .primary .exhibit-item a').addClass('th');
-      jQuery('#atwood-layout .secondary .exhibit-item a').addClass('th');
-      jQuery('#sort-links .sort-label').prependTo('#sort-links-list');
-      jQuery('#itemfiles .image-jpeg a').addClass('th');
-      jQuery('.sub-nav .sorting').addClass('active');
-      jQuery('#recent-items .item-title').each(function() {
-        jQuery(this).prev('.item-img').children('.th').append(this);
-      });
-      if (jQuery('#side-gallery').length) {
-        jQuery('#side-gallery .exhibit-item').each(function(){
-          jQuery(this).append('<a class="caption"/>');
-          var itemLink = jQuery(this).find('.exhibit-item-link').attr('href');
-          var itemCaption = jQuery(this).find('.permalink').attr('title');
-          jQuery(this).find('.caption').attr('href',itemLink).html(itemCaption);
-        })
-      }
-    });
-  </script>
+  <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/foundation/5.4.0/js/vendor/modernizr.js"></script>
 </head>
 <?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
   <?php fire_plugin_hook('public_body', array('view'=>$this)); ?>
